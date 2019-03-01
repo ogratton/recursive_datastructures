a6b4894c51fbfb303f1e87260b4464d22740252a
Oliver Gratton <ojgratton@gmail.com>
2019-03-01 00:43:01 +0000

    prefix tree functions

--- prefix_tree.py
+++ prefix_tree.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class PrefixTree:
 
     class End(object):
@@ -20,31 +23,39 @@ class PrefixTree:
             self.branches.append((head, PrefixTree(tails)))
 
 
-def size(tree: PrefixTree):
-        return tree.is_end + sum(map(lambda x: size(x), tree.branches))
+def size(tree: PrefixTree) -> int:
+    return tree.is_end + sum(map(lambda x: size(x[1]), tree.branches))
 
 
-def height(tree: PrefixTree):
+def height(tree: PrefixTree) -> int:
+    if not tree.branches:
+        return 0
+    return max([1 + height(t) for _, t in tree.branches])
+
+
+def strings(tree: PrefixTree) -> List[str]:
+    # TODO broken
+    if tree.is_end:
+        missing = [[c+s for s in strings(t)] for c, t in tree.branches]
+        if missing:
+            print(missing)
+        return [""]
+    return [c+tail for c, t in tree.branches for tail in strings(t)]
+
+
+def subtree(tree: PrefixTree) -> PrefixTree:
     ...
 
 
-def strings(tree: PrefixTree):
+def has(tree: PrefixTree) -> bool:
     ...
 
 
-def subtree(tree: PrefixTree):
+def predict(tree: PrefixTree) -> List[str]:
     ...
 
 
-def has(tree: PrefixTree):
-    ...
-
-
-def predict(tree: PrefixTree):
-    ...
-
-
-def ptree_repr(tree: PrefixTree):
+def ptree_repr(tree: PrefixTree) -> str:
     # TODO make nicer
     if tree.is_end:
         return repr(PrefixTree.end) + repr(tree.branches)
@@ -56,7 +67,11 @@ PrefixTree.__repr__ = ptree_repr
 
 
 if __name__ == "__main__":
-    w = ["cat", "category", "catapult", "cocoa", "dog", "dogma", "mouse", "tree"]
-    t = PrefixTree(w)
+    wo = ["cat", "category", "categories", "catapult", "cocoa", "dog", "dogma", "mouse", "tree", "trees"]
+    # wo = ["a"]
+    tr = PrefixTree(wo)
 
-    print(t)
+    print(tr)
+    print(size(tr))
+    print(height(tr))
+    print(strings(tr))
